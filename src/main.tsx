d5895372ac5cb2e1ec1b9f31f4c7dc922eb660d1
Kevin200519 <Ksramirez@ucundinamarca.edu.co>
2024-10-28 15:13:19 -0500

    agree images and solution main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import ExhibitionsScreens from "./Components/screens/exhibitionScreen/ExhibitionsScreen.tsx";
+import CollectionInventoryScreen from "./Components/screens/collectionInventoryScreen/CollectionInventoryScreen.tsx";
+import App from "./App.tsx";
+import HomeScreen from "./Components/screens/homeScreen/HomeScreen.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<App />}>
+        <Route index path="/" element={<HomeScreen />} />
+
+        <Route path="/Exhibition" element={<ExhibitionsScreens />} />
+        <Route
+          path="/collectInventory"
+          element={<CollectionInventoryScreen />}
+        />
+      </Route>
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
