0b51a1ba49008e7a3971f03883ad6a340ff742fb
Kevin200519 <Ksramirez@ucundinamarca.edu.co>
2024-10-30 18:08:39 -0500

    agree new items

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import "./App.css";
 import Navbar from "./Components/navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import {MoonFilled, UserOutlined, PoweroffOutlined} from"@ant-design/icons";
 function App() {
   return (
     <section className="layout">
@@ -9,6 +10,9 @@ function App() {
         <Navbar />
       </header>
       <main>
+      <MoonFilled style={{fontSize : '25px',  display: 'flex', gap: '15px',flexDirection : 'column'  }}/>
+      <UserOutlined style={{fontSize : '25px', display: 'flex', gap: '10px',flexDirection : 'column'  }}/>
+      <PoweroffOutlined style={{fontSize : '25px', display: 'flex', gap: '10px',flexDirection : 'column' }}/>
         <Outlet />
       </main>
       <footer>Museo</footer>
