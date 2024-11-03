6276dc52df607bff50e11f8bb6aa93e12845b9b9
Kevin200519 <Ksramirez@ucundinamarca.edu.co>
2024-11-02 22:01:24 -0500

    import new libraries and forms of the login

--- src/Components/screens/loginScreen/LoginScreen.tsx
+++ src/Components/screens/loginScreen/LoginScreen.tsx
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
 import { UserOutlined, MailOutlined, LockOutlined } from '@ant-design/icons'; // Importa íconos de Ant Design
-import { Button } from 'antd'; // Importa el botón de Ant Design
+import { Button, Input } from 'antd'; // Importa el botón de Ant Design
 import './LoginScreen.css'; // Importa los estilos CSS
+import { Controller, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { loginSchema } from '../../../assets/lib/zod/validations';
+import { object } from 'zod';
 
-
+interface IloginForm {
+  email: string;
+  password: string;
+}
 const LoginScreen: React.FC = () => {
   const [email, setEmail] = useState<string>(''); // Estado para el correo electrónico
   const [password, setPassword] = useState<string>(''); // Estado para la contraseña
+  const { control, handleSubmit, formState: { errors } } = useForm<IloginForm>({ defaultValues: { email: "", password: "" }, resolver: zodResolver(loginSchema) })
 
   // Maneja el evento de inicio de sesión
-  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault(); // Previene el comportamiento por defecto del formulario
-    console.log('Iniciar sesión:', { email, password }); // Imprime el email y la contraseña en la consola
+  const handleLogin = (formlogin: IloginForm) => {
+    console.log('Iniciar sesión:', formlogin); // Imprime el email y la contraseña en la consola
   };
 
   // Maneja el evento de registro
   const handleRegister = () => {
     console.log('Ir a Registrarse'); // Imprime un mensaje en la consola
   };
-  
+
   return (
     <div className="login-container"> {/* Contenedor principal */}
       <div className="login-box"> {/* Caja de login */}
@@ -26,30 +33,30 @@ const LoginScreen: React.FC = () => {
           <UserOutlined className="user-icon" /> Bienvenido {/* Título de bienvenida */}
         </h2>
         <p className="subtitle">Inicia sesión en tu cuenta</p> {/* Subtítulo */}
-        <form onSubmit={handleLogin}> {/* Formulario */}
+        <form onSubmit={handleSubmit(handleLogin)}> {/* Formulario */}
           <div className="input-group"> {/* Grupo para el campo de email */}
             <label htmlFor="email">
               <MailOutlined className="icon" /> Correo Electrónico
             </label>
-            <input
-              type="email" // Tipo de input para email
-              id="email"
-              value={email} // Valor del estado email
-              onChange={(e) => setEmail(e.target.value)} // Actualiza el estado email al cambiar
-              required // Campo requerido
+            <Controller control={control}
+              name="email"
+              render={({ field }) => <Input {...field}// destructuracion {...field}
+                placeholder='pedroelcapito@gmail.com'
+              />}
             />
+            {errors.email ? <p>{errors.email.message}</p> : null}
           </div>
           <div className="input-group"> {/* Grupo para el campo de contraseña */}
             <label htmlFor="password">
               <LockOutlined className="icon" /> Contraseña
             </label>
-            <input
-              type="password" // Tipo de input para contraseña
-              id="password"
-              value={password} // Valor del estado password
-              onChange={(e) => setPassword(e.target.value)} // Actualiza el estado password al cambiar
-              required // Campo requerido
+            <Controller control={control}
+              name='password'
+              render={({ field }) => <Input.Password {...field}
+                placeholder="*****"
+              />}
             />
+            {errors.password ? <p>{errors.password.message}</p> : null}
           </div>
           {/* Botón de Ant Design para iniciar sesión */}
           <Button type="primary" htmlType="submit" className="login-button">
